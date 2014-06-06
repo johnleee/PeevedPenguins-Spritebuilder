bddd2df315edaf4629e7f91cdb8b3b02ba33ab2c
John Lee <john.lee@sv.cmu.edu>
2014-06-05 19:06:21 -0700

    add files

---
+++ Source/Penguin.m
@@ -0,0 +1,24 @@
+//
+//  Penguin.m
+//  PeevedPenguins
+//
+//  Created by John Lee on 6/5/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "Penguin.h"
+
+@implementation Penguin
+
+- (id)init {
+    self = [super init];
+    
+    if (self) {
+        CCLOG(@"Penguin created");
+    }
+    
+    return self;
+}
+
+
+@end
